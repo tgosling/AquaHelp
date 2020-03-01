2685e67c12c3b03e8ea249ba0d963bff0c68342f
Tyler Gosling <t.gosling61@gmail.com>
2020-03-01 13:47:57 -0500

    Camp Bookings page updated and stlyed v0.59

--- src/front_end/Camps/BookingForm.js
+++ src/front_end/Camps/BookingForm.js
@@ -12,31 +12,51 @@ const BookingForm = (props) => {
             <h2>Camp Booking Form</h2>
         </Jumbotron>
         <Form>
-            <FormGroup>
-                <Label for="campName">Camp Name:</Label>
+            <FormGroup row>
+                <Label for="campName" sm={2}>Camp Name:</Label>
                 <Col sm={10}>
-                    <Input type="textarea" name="campName" id="campID" placeholder="Enter Camp Name..."/>
+                    <Input type="" name="camp" id="campName" placeholder="Enter Camp Name..."/>
                 </Col>
             </FormGroup>
-            <FormGroup>
-                <Label for="pools">Pool:</Label>
+            <FormGroup row>
+                <Label for="councName" sm={2}>Councillor Name:</Label>
+                <Col sm={10}>
+                    <Input type="" name="councillor" id="councName" placeholder="Enter Councillor Name..."/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="campPhone" sm={2}>Phone #:</Label>
+                <Col sm={10}>
+                    <Input type="number" name="campNum" id="campPhone" placeholder="Enter Camp Contact Number..."/>
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Label for="pools" sm={2}>Pool:</Label>
+                <Col sm={10}>
                 <Input type="select" name="selectPool" id="selectedPool">
                     <option>Canada Games Aquatic Centre</option>
                     <option>Carling Heights Optimist Community Center</option>
                     <option>South London Community Pool</option>
                 </Input> 
+                </Col>
             </FormGroup>
-                {/*React datepicker*/}
-            <FormGroup>
-                <Label for="campers">Number of Campers:</Label>
-                <Input type="textArea" name="numCampers" id="campers"/>
+                {/*React 
+                   datepicker
+                   goes here*/}
+            <FormGroup row>
+                <Label for="campers" sm={2}>Number of Campers:</Label>
+                <Col sm={10}>
+                    <Input  name="numCampers" id="campers"/>
+                </Col>
             </FormGroup>
-            <FormGroup>
-                <Label for="campers">Number of Councillors:</Label>
-                <Input type="textArea" name="numCampers" id="campers"/>
+            <FormGroup row>
+                <Label for="campers" sm={2}>Number of Councillors:</Label>
+                <Col sm={10}>
+                <Input name="numCampers" id="campers"/>
                 <FormText color="muted">
                     Remember, there is a 1:8 counsellor to camper ratio for swimming.
                 </FormText>
+                </Col>
             </FormGroup>
             <Button>Submit</Button>
         </Form>
