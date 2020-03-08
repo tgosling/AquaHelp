6edbfbba57b6c87287d9e5527f223d807a8ac75d
Tyler Gosling <t.gosling61@gmail.com>
2020-03-08 11:26:51 -0400

    Initial styling of CampAvail page, test table and progress bar

--- src/front_end/Camps/CampAvail.js
+++ src/front_end/Camps/CampAvail.js
@@ -1,35 +1,86 @@
 import React, { useState } from "react";
 import Page from "../Page/Page";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import {
- Table,
- Progress,
- Button
+  Table,
+  Progress,
+  Button,
+  InputGroup,
+  InputGroupAddon,
+  Jumbotron
 } from "reactstrap";
 
-const CampAvail = (props) => {
-    return(
-        <Page>
-            <h1>Pool Availablilty</h1>
-            <div/>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>Pool</th>
-                        <th>Availability</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row" width="30%">Canada Games Aquatic Center</th>
-                            <td><Progress value="25">25%</Progress></td>
-                            <td><Button>Book Camp</Button></td>
-                        </tr>
-                    </tbody>
-               
-            </Table>
-        </Page>
-    );
-}//end CampAvail
+const CampAvail = props => {
+  const [startDate, setStartDate] = useState(null);
+  return (
+    <Page>
+      <Jumbotron>
+        <h1>Pool Availablilty</h1>
+        <div />
+        <InputGroup>
+          <InputGroupAddon>Select a Date:</InputGroupAddon>
+          <DatePicker
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            minDate={new Date()}
+            placeholderText="Check date"
+            style={{margin: "2%"}}
+          />
+        </InputGroup>
+      </Jumbotron>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Pool</th>
+            <th>Availability</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row" width="20%">
+              Canada Games Aquatic Center
+            </th>
+            <td width="35%">
+              <Progress value="70" style={{ height:"25px" }}>
+                70%
+              </Progress>
+            </td>
+            <td width="10%" height="100%">
+              <Button>Book</Button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" width="20%">
+              Carling Heights 
+            </th>
+            <td width="35%">
+              <Progress value="25" style={{ height:"25px" }}>
+                25%
+              </Progress>
+            </td>
+            <td width="10%" height="100%">
+              <Button>Book</Button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row" width="20%">
+              South London Community Pool
+            </th>
+            <td width="35%">
+              <Progress value="55" style={{ height:"25px" }}>
+                55%
+              </Progress>
+            </td>
+            <td width="10%" height="100%">
+              <Button>Book</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Page>
+  );
+}; //end CampAvail
 
 export default CampAvail;
